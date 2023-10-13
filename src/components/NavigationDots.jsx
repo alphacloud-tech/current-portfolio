d7cf22a482e3365df097877922b1999cbe3ac72e
alphacloud-tech <adesanjo470@gmail.com>
2023-10-13 17:00:07 +0100

    update

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -17,21 +17,20 @@ import React from 'react'
 
 const NavigationDots = ({ active }) => {
     return (
-      <div className='app__navigation'>
-        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
-          <a
-            href={`#${item}`}
-            key={item + index}
-            className="app__navigation-dot"
-            style={active === item ? { backgroundColor: '#313BAC' } : {}}
-            aria-label={`Navigate to ${item}`} // Provide an accessible label
-            role="button"
-            tabIndex="0"
-          />
-        ))}
-      </div>
+        <div className='app__navigation'>
+            {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+                <a
+                    href={`#${item}`}
+                    key={item + index}
+                    className="app__navigation-dot"
+                    style={active === item ? { backgroundColor: '#313BAC' } : {}}
+                >
+                    <span className="visually-hidden">Navigate to {item}</span>
+                </a>
+            ))}
+        </div>
     );
-  };
+};
 
 
 export default NavigationDots
