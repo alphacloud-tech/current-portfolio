cf285fdc48980b46579816c6e9bd3b16a9cebe23
alphacloud-tech <adesanjo470@gmail.com>
2023-10-13 15:51:28 +0100

    first commit

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 
+// const NavigationDots = ({ active }) => {
+//   return (
+//     <div className='app__navigation'>
+//         {['home', 'about', 'work', 'skills', 'testimonial', 'contact', ].map((item, index) => (
+//             <a
+//                 href={`#${item}`}
+//                 key = {item + index}
+//                 className="app__navigation-dot"
+//                 style={active === item ? {backgroundColor: '#313BAC'} : { }}
+//             />
+//         ))}
+//     </div>
+//   )
+// }
+
 const NavigationDots = ({ active }) => {
-  return (
-    <div className='app__navigation'>
-        {['home', 'about', 'work', 'skills', 'testimonial', 'contact', ].map((item, index) => (
-            <a 
-                href={`#${item}`}
-                key = {item + index} 
-                className="app__navigation-dot"
-                style={active === item ? {backgroundColor: '#313BAC'} : { }}
-            />
+    return (
+      <div className='app__navigation'>
+        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: '#313BAC' } : {}}
+            aria-label={`Navigate to ${item}`} // Provide an accessible label
+            role="button"
+            tabIndex="0"
+          />
         ))}
-    </div>
-  )
-}
+      </div>
+    );
+  };
+
 
 export default NavigationDots
